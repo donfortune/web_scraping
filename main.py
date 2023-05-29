7ce6e73519c57f77e943704818651557c47b2947
donfortune <donfortunet.df@gmail.com>
2023-05-29 13:11:13 +0100

    configure oop

--- main.py
+++ main.py
@@ -11,35 +11,37 @@ URL = "http://programmer100.pythonanywhere.com/tours/"
 connection = sqlite3.connect("data.db")
 
 
-def scrape(url):
-    response = requests.get(URL)
-    content = response.text
-    return content
+class Tours:
+    def scrape(self, url):
+        response = requests.get(URL)
+        content = response.text
+        return content
 
-def get_source(source):
-    extractor = selectorlib.Extractor.from_yaml_file("source.yaml")
-    value = extractor.extract(source)['tours']
-    with open('tours.txt', 'a')  as file:
-        file.write(value + '\n')
-    return value
-
-
-def send_email():
-    host = "smtp.gmail.com"
-    port = 465
-    username = 'donfortunet.df@gmail.com'
-    password = 'dqpeojyayhvpcvcy'
-    receiver_email = 'osowoayiobi@gmail.com'
-    message = EmailMessage()
-    message['Subject'] = 'New Location Found'
-    message.set_content(extracted_info)
+    def get_source(self, source):
+        extractor = selectorlib.Extractor.from_yaml_file("source.yaml")
+        value = extractor.extract(source)['tours']
+        with open('tours.txt', 'a')  as file:
+            file.write(value + '\n')
+        return value
 
 
-    context = ssl.create_default_context()
-    with smtplib.SMTP_SSL(host, port, context=context) as file:
-        file.login(username, password)
-        file.sendmail(username, receiver_email, message.as_string())
-        file.quit()
+class SendMail:
+    def send_email(self):
+        host = "smtp.gmail.com"
+        port = 465
+        username = 'donfortunet.df@gmail.com'
+        password = 'dqpeojyayhvpcvcy'
+        receiver_email = 'osowoayiobi@gmail.com'
+        message = EmailMessage()
+        message['Subject'] = 'New Location Found'
+        message.set_content(extracted_info)
+
+
+        context = ssl.create_default_context()
+        with smtplib.SMTP_SSL(host, port, context=context) as file:
+            file.login(username, password)
+            file.sendmail(username, receiver_email, message.as_string())
+            file.quit()
 
 def store_to_database(get_source):
     row = get_source.split(',')
@@ -62,14 +64,16 @@ def read_data_db(get_source):
 
 if __name__ == "__main__":
     while True:
-        scrapped = scrape(URL)
-        extracted = get_source(scrapped)
+        tours = Tours()
+        scrapped = tours.scrape(URL)
+        extracted = tours.get_source(scrapped)
         extracted_info = f"The loaction and date for the next tour is: {extracted}"
         print(extracted_info)
 
         if extracted != 'No upcoming tours':
             if extracted not in 'tours.txt':
-                send_email()
+                sendmail = SendMail()
+                sendmail.send_email()
 
         time.sleep(2)
 
